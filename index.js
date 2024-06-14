3ea7c3f5388f9f4495b1cd3a509d99bf71fedf4e
kingofficerbotsdevelopemelllll <ededeeded79@gmail.com>
2024-06-14 17:21:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Customize your bot's status by forking this."];
+const statusMessages = ["Playing KingOfficer Bots"];
 
 
 let currentIndex = 0;
