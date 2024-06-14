b81a8c4ce689e86f43615aaeb8d67c79c6f3b261
kingofficerbotsdevelopemelllll <ededeeded79@gmail.com>
2024-06-14 17:40:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,58 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Playing KingOfficer Bots"];
+const statusMessages = ["Playing KingOfficer Bos=ts"];
 
 
 let currentIndex = 0;
 const channelId = '';
 
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|  ✅ Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+
+
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'dnd',
+  });
+
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+   
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
+client.once('ready', () => {
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    💉 Status Changing Code Injected Succesfully`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
+});
+
+login();
 async function login() {
   try {
     await client.login(process.env.TOKEN);
